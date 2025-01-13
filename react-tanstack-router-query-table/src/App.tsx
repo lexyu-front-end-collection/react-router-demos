c4e92f546f08bd1f508611d06c173ad991ba67ba
LexYu <84486286+TLexYuW@users.noreply.github.com>
2025-01-13 13:39:53 +0800

    update

--- react-tanstack-router-query-table/src/App.tsx
+++ react-tanstack-router-query-table/src/App.tsx
@@ -1,11 +1,26 @@
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from './routeTree.gen'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface RegisterRouter {
+    router: typeof router;
+  }
+}
+
+const queryClient = new QueryClient();
 
 function App() {
 
   return (
     <>
-      <h1>TanStack Router + Query + Table</h1>
+      <QueryClientProvider client={queryClient}>
+        <h1>TanStack Router + Query + Table</h1>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </>
   )
 }
