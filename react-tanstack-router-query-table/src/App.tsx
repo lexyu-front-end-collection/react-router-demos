8b97ae2a63d1efbf1b6ddc982c76ff8d0b1b018b
LexYu <84486286+TLexYuW@users.noreply.github.com>
2025-01-13 11:50:46 +0800

    update

---
+++ react-tanstack-router-query-table/src/App.tsx
@@ -0,0 +1,13 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>TanStack Router + Query + Table</h1>
+    </>
+  )
+}
+
+export default App
